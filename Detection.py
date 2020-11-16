4ca67a56c102bf8c6fd773975a8437dd0db5ff79
Gabriel <gabriel.rmachado10@gmail.com>
2020-11-15 23:38:03 -0300

    Detection Module implemented.

--- Detection.py
+++ Detection.py
@@ -1,4 +1,5 @@
 from Helper import Helper
+import numpy as np
 
 class Detection:
     def __init__(self, Vm, Tb):
@@ -6,4 +7,14 @@ class Detection:
         self.__tb = Tb
 
     def detect(self):
-        pass
+        qleg = 0
+        qadv = 0
+
+        for i in range(len(self.__vm)):
+            if self.__vm[i] <= self.__tb[i]: 
+                qleg = qleg + 1 # voted as legitimate by member 'i' of the ensemble
+            else:
+                qadv = qadv + 1 # otherwise
+
+        if qleg > qadv: return True
+        else: return False
