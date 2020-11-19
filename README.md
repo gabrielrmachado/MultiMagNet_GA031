b4003ce025db9c964cb62a9d697909d360280c2b
Gabriel <gabriel.rmachado10@gmail.com>
2020-11-19 12:57:07 -0300

    Starting the  implementation of Data module's tests

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Install Dependencies
+# MultiMagNet (Disciplina GA-031)
+
+_Código-fonte e implementação dos casos de testes relatados no Trabalho 2._
+
+## Instalando as dependências
 
 ```
 $ sudo pip3 install python-mnist
@@ -8,4 +12,31 @@ $ sudo pip3 install numpy
 ```
 ```
 $ sudo pip3 install sklearn
+```
+## Executando o _MultiMagNet_
+
+1. Adicione a pasta _modules_ no `PYTHONPATH`:
+
+```
+$ export PYTHONPATH="$PYTHONPATH:/path_to_project_folder/modules"
+```
+
+2. Na pasta do projeto (`MultiMagNet_GA031`), execute o seguinte comando no terminal para iniciar o _MultiMagNet_:
+
+```
+$ python3 modules/__main__.py
+```
+
+# Running the tests
+
+1. Adicione a pasta _tests_ no no `PYTHONPATH`:
+
+```
+$ export PYTHONPATH="$PYTHONPATH:/path_to_project_folder/tests"
+```
+
+2. Na pasta do projeto (`MultiMagNet_GA031`), execute o seguinte comando no terminal para iniciar os testes do _MultiMagNet_:
+
+```
+$ python3 tests/__main__.py
 ```
--- tests/test_data_unit.py
+++ tests/test_data_unit.py
@@ -1,17 +1,28 @@
 import unittest
+import numpy as np
 from modules.Data import ImageDAO, UserDAO, Dataset, AttackAlgorithm
 
 class TestData(unittest.TestCase):
     
-    def setUp(self):
-        self._number_legitimate_images = 200
-
-    def test_number_legitimate_images_and_labes_vleg_set(self):
-        images, labels = ImageDAO.get_images(self._number_legitimate_images)
-        self.assertEqual(len(images), self._number_legitimate_images)
-        self.assertEqual(len(labels), self._number_legitimate_images)
+    def test_number_images_and_labels_vleg_set(self):
+        number_legitimate_images = 153
+        images_vleg, labels_vleg = ImageDAO.get_images(number_legitimate_images)
+        self.assertEqual(len(images_vleg), number_legitimate_images)
+        self.assertEqual(len(labels_vleg), number_legitimate_images)
 
     def test_number_images_and_labels_v_set(self):
-        vdata, vlabels = ImageDAO.get_Vdataset(self._number_legitimate_images, AttackAlgorithm.FGSM, eps=0.3)
-        self.assertEqual(len(vdata), self._number_legitimate_images*2)
-        self.assertEqual(len(vlabels), self._number_legitimate_images*2)
+        number_legitimate_images = 153
+        images_v, labels_v = ImageDAO.get_Vdataset(number_legitimate_images, AttackAlgorithm.FGSM, eps=0.3)
+        self.assertEqual(len(images_v), number_legitimate_images*2)
+        self.assertEqual(len(labels_v), number_legitimate_images*2)
+
+    def test_v_set_is_balanced(self):
+        number_legitimate_images = 153
+        _, labels_v = ImageDAO.get_Vdataset(number_legitimate_images, AttackAlgorithm.FGSM, eps=0.3)
+        unique, counts = np.unique(labels_v, return_counts=True)
+        c = dict(zip(unique, counts))
+        self.assertEqual(c[0], c[1])
+
+    def test_load_users_from_file(self):
+        userDao = UserDAO()
+        self.assertEqual(userDao.get_users_size()-1, 2)
